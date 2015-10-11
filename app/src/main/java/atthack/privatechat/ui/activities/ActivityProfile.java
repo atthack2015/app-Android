8ea0ff0883559adbca740d80b2d1305bfcda34ce
LeonardoPC23 <pereyraleonardo23@gmail.com>
2015-10-10 19:06:42 -0500

    Paso 5

---
+++ app/src/main/java/atthack/privatechat/ui/activities/ActivityProfile.java
@@ -0,0 +1,18 @@
+package atthack.privatechat.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import atthack.privatechat.R;
+import atthack.privatechat.ui.commons.BaseActivity;
+
+/**
+ * Created by LEONARDO on 10/10/2015.
+ */
+public class ActivityProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_user);
+    }
+}
